2c261bb6839fa1230abac0d24d31dc03753359dd
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-08 09:47:53 +0300

    Data Annotations

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 
 namespace CodeFirst
 {
     public class Course
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(2000)]
         public string Description { get; set; }
         public CourseLevel Level { get; set; }
         public float FullPrice { get; set; }
+        [ForeignKey("Author")]
+        public int AuthorId {  get; set; }
         public Author Author { get; set; }
         public IList<Tag> Tags { get; set; }
     }
